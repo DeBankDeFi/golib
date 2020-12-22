03eea588f550bac559aade2890d90717b1a0886c
barryz <barryzxb@gmail.com>
2020-12-22 22:31:59 +0800

    chore: use glog lib finnaly

--- cmdhelper/version.go
+++ cmdhelper/version.go
@@ -1,11 +1,11 @@
 package cmdhelper
 
 import (
+	"fmt"
+
 	"github.com/DeBankDeFi/golib/shared"
 
-	"github.com/XSAM/go-hybrid/log"
 	"github.com/spf13/cobra"
-	"go.uber.org/zap"
 )
 
 func Version() *cobra.Command {
@@ -13,7 +13,7 @@ func Version() *cobra.Command {
 		Use:   "version",
 		Short: "Print version information",
 		Run: func(cmd *cobra.Command, args []string) {
-			log.BgLogger().Info("app info", zap.Any("info", shared.AppInfo()))
+			fmt.Println(shared.AppInfo())
 		},
 	}
 	return &cmd
